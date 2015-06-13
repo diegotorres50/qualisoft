32302b1043ec76645f351a07b6d497f14bbe7223
Diego Torres <diegotorres50@hotmail.com>
2015-06-13 09:39:28 -0500

    Renombamos el controlador UserArea a Security

--- dev/src/Qualisoft/AppBundle/Controller/SecurityController.php
+++ dev/src/Qualisoft/AppBundle/Controller/SecurityController.php
@@ -5,14 +5,14 @@ namespace Qualisoft\AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route; //@diegotorres50: necesario para las anotaciones de rutas 
 
-class UserAreaController extends Controller
+class SecurityController extends Controller
 {
 	 /**
-     * @Route("/login", name="qualisoft_app_userarea_login") 
+     * @Route("/login", name="qualisoft_security_login") 
      */
     public function loginAction()
     {
-        return $this->render('QualisoftAppBundle:UserArea:login.html.twig', array('var' => 'Login aquí'));
+        return $this->render('QualisoftAppBundle:Security:login.html.twig', array('var' => 'Login aquí'));
     }
       
 }
